e5d9528ba7b8327ec8df6890b91f661cdcb90a1c
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-09-17 23:20:19 +0200

    Initialize Spring Boot banking API

---
+++ src/main/java/com/phakiso/enterprisebankingapi/EnterpriseBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.phakiso.enterprisebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseBankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phakiso/enterprisebankingapi/EnterpriseBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phakiso.enterprisebankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
