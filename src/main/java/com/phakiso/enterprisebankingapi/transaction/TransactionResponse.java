76c1083f113da8a68ef0e099a735864e68860e38
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-09-20 12:18:27 +0200

    Complete transaction response DTO

--- src/main/java/com/phakiso/enterprisebankingapi/transaction/TransactionResponse.java
+++ src/main/java/com/phakiso/enterprisebankingapi/transaction/TransactionResponse.java
@@ -1,4 +1,14 @@
 package com.phakiso.enterprisebankingapi.transaction;
 
-public class TransactionResponse {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record TransactionResponse(
+        Integer transactionId,
+        Integer accountNumber,
+        String transactionType,
+        BigDecimal amount,
+        BigDecimal balanceAfter,
+        LocalDateTime transactionDate
+) {
 }
