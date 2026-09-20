3b619cb0000139f12f8b7977fb9ccb5fe6d0f042
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-09-20 20:32:12 +0200

    Add deposit request DTO

---
+++ src/main/java/com/phakiso/enterprisebankingapi/account/DepositRequest.java
@@ -0,0 +1,15 @@
+package com.phakiso.enterprisebankingapi.account;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record DepositRequest(
+
+        @NotNull(message = "Deposit amount is required")
+        @DecimalMin(value = "0.01", message = "Deposit amount must be greater than zero")
+        BigDecimal amount
+
+) {
+}
