d173ecb86e39a58b852577be6081f4c0662594e9
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-09-20 12:49:16 +0200

    Add transaction history controller

---
+++ src/main/java/com/phakiso/enterprisebankingapi/transaction/TransactionController.java
@@ -0,0 +1,25 @@
+package com.phakiso.enterprisebankingapi.transaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/accounts/{accountNumber}/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public List<TransactionResponse> getTransactions(
+            @PathVariable Integer accountNumber) {
+        return transactionService.getTransactionsByAccountNumber(accountNumber);
+    }
+}
