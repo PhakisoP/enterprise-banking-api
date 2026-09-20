47dba60fe5ddcbe66cf659d8be3f7b37bdf9f923
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-09-20 11:48:40 +0200

    Add account transaction query

--- src/main/java/com/phakiso/enterprisebankingapi/transaction/TransactionRepository.java
+++ src/main/java/com/phakiso/enterprisebankingapi/transaction/TransactionRepository.java
@@ -2,5 +2,9 @@ package com.phakiso.enterprisebankingapi.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+
+    List<Transaction> findByAccountNumber(Integer accountNumber);
 }
